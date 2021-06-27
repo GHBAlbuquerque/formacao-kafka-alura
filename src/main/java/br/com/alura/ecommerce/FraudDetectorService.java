0011eaa7b0845fafa08766caf5e9832ec4abb2b6
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2021-06-27 16:16:59 -0300

    aula 01 - final

---
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -0,0 +1,44 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class FraudDetectorService {
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        while(true) {
+            var records = consumer.poll(Duration.ofMillis(5000));
+            if (records.isEmpty()) {
+                System.out.println("Náo encontrei registros!");
+            } else {
+                System.out.println("Processing "+ records.count()+" records...");
+                for (var record : records) {
+                    System.out.println("----------------");
+                    System.out.println("record:" + record.key() + "/" + record.value() + "/" + record.partition() + "/" + record.offset());
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Order processed!");
+                }
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        return properties;
+    }
+}
